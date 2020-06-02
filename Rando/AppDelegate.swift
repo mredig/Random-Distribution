d865055d64bd050631d5229a6ae3b394fa1b4ccd
Michael Redig <mredig@gmail.com>
2020-06-02 16:01:26 -0500

    initial commit

---
+++ Rando/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Rando
+//
+//  Created by Michael Redig on 6/2/20.
+//  Copyright © 2020 Red_Egg Productions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ Rando/GameViewController.swift
@@ -0,0 +1,128 @@
+//
+//  GameViewController.swift
+//  Rando
+//
+//  Created by Michael Redig on 6/2/20.
+//  Copyright © 2020 Red_Egg Productions. All rights reserved.
+//
+
+import SceneKit
+import QuartzCore
+
+class GameViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 3)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = NSColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+		let cube = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0)
+
+		let mat = SCNMaterial()
+		mat.transparency = 0.5
+		mat.isDoubleSided = true
+		mat.lightingModel = .physicallyBased
+
+		let scnCube = SCNNode(geometry: cube)
+		cube.insertMaterial(mat, at: 0)
+		scene.rootNode.addChildNode(scnCube)
+		cameraNode.constraints = [SCNLookAtConstraint(target: scnCube)]
+
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = NSColor.black
+        
+        // Add a click gesture recognizer
+        let clickGesture = NSClickGestureRecognizer(target: self, action: #selector(handleClick(_:)))
+        var gestureRecognizers = scnView.gestureRecognizers
+        gestureRecognizers.insert(clickGesture, at: 0)
+        scnView.gestureRecognizers = gestureRecognizers
+
+		populate(inside: scnCube)
+    }
+
+	func populate(inside cubeNode: SCNNode) {
+		let cube = SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0)
+		let mat = SCNMaterial()
+		mat.lightingModel = .phong
+
+		let randVal = { CGFloat.random(in: 0...1) - 0.5 }
+
+		(0...10000).forEach { _ in
+			let node = SCNNode(geometry: cube)
+			node.position = .init(randVal(), randVal(), randVal())
+			cubeNode.addChildNode(node)
+		}
+	}
+    
+    @objc
+    func handleClick(_ gestureRecognizer: NSGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // check what nodes are clicked
+        let p = gestureRecognizer.location(in: scnView)
+        let hitResults = scnView.hitTest(p, options: [:])
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            
+            // get its material
+            let material = result.node.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = NSColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = NSColor.red
+            
+            SCNTransaction.commit()
+        }
+    }
+}
